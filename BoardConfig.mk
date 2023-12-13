79fd0151f20f23bbe71424d21091c55b881d8a97
Noah113-Allen <kingfighter2958@gmail.com>
2023-12-13 20:08:41 +0530

    light: Define Partitions Signed-off-by: Noah113-Allen <kingfighter2958@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,11 @@
 
 DEVICE_PATH := device/xiaomi/light
 
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+
+BOARD_SUPER_PARTITION_SIZE := 9126805504
+BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200
+
 # Inherit from mt6833-common
 include device/xiaomi/mt6833-common/BoardConfigCommon.mk
