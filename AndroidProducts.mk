fe0a61c57cddfa84be28ae524fef6f0440e5dfc5
Noah113-Allen <kingfighter2958@gmail.com>
2023-12-13 19:35:10 +0530

    light: Initial skeleton tree Signed-off-by: Noah113-Allen <kingfighter2958@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_light.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_light-user \
+    lineage_light-userdebug \
+    lineage_light-eng
---
+++ proprietary-files.sh
@@ -0,0 +1,1 @@
+# All blobs from light-user-12-SP1A.210812.016-V13.0.11.0.SLSMIXM-release-keys, unless pinned
